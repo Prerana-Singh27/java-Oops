246f1747d4a14a27068d9e5e63c11909aae027d0
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-02 11:45:55 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -57,3 +57,44 @@ class BankAccount {
               return amount;
           }
       }
+
+class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    Employee(double salary, int age, String name) {
+        this.salary = salary;
+        this.age = age;
+        this.name = name;
+    }
+
+    String getEmployeeDetails() {
+        return "EmployeeDetails: Name:" + name +", Age: " + age + ", salary: " +salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
