3ddef20cde2e103073de00a0854c7c4a796ed988
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-03 21:52:59 +0530

    Update and rename Inheritance to Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+//start base class libraryitem --attributes--method--
+
+public class LibraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+
+
+    public void checkout() {
+        System.out.println("checkout");
+
+    }
+
+    public void returnItem() {
+        System.out.println("returning the item");
+    }
+}
+
