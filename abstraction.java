4cce987a125cfa1d70fe8bfce68bca08d0395248
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-05 18:30:39 +0530

    Update abstraction.java

--- abstraction.java
+++ abstraction.java
@@ -1,1 +1,4 @@
 //abstract class Shape--abstract method calculateArea(). 2 subclasses Circle, square--attributes like radius,sides
+public abstract class Shape {
+    public abstract double calculateArea();
+}
