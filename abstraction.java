749569fff309cfbb1aad403caac589e0df67dc99
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-05 18:32:29 +0530

    Update abstraction.java

--- abstraction.java
+++ abstraction.java
@@ -20,3 +20,22 @@ public  class Circle extends Shape {
         return Math.PI * Math.pow(radius,2);
     }
 }
+
+//subclasses 2
+public  class Square extends Shape  {
+    private final  double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+}
+
