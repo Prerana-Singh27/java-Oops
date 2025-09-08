3279aa15d40dcc8467aa697a279d855772b5c2ec
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-08 11:53:04 +0530

    Update polymorphism.java

--- polymorphism.java
+++ polymorphism.java
@@ -1,1 +1,23 @@
 //class calculator--multiple add() methods ---add sum 2 integers, 3 integers or two doubles. diff no of parameters ..
+public class Calculator {
+
+    public static int add(int a , int b) {
+        return a+b;
+    }
+
+    public static int add(int a , int b , int c) {
+        return a + b + c;
+    }
+
+    public static double add(double a , double b ) {
+        return a + b ;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5,6));
+        System.out.println(calc.add(5,6 ,7));
+        System.out.println(calc.add(5.9,6.2));
+    }
+}
+
