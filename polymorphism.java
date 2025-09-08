aa3f205cece2e846d2f06aab0b6db6336ffec61d
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-08 12:02:00 +0530

    Update polymorphism.java

--- polymorphism.java
+++ polymorphism.java
@@ -30,3 +30,12 @@ public class Vehicle {
 
     }
 }
+
+public class Car extends Vehicle {
+
+    @Override
+    public void service() {
+        super.service();
+        System.out.println("CAR IS GETTING SERVICED...");
+    }
+}
