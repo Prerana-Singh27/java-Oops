9209ad9b739693e64a91dd712a6b29d7e8af62b1
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-30 16:07:46 +0530

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver {
     public static void main(String[] args){
-//    car myCar = new car();
+//   car myCar = new car();
 //    myCar.addFuel(6);
 //    myCar.drive();
 //        myCar.drive();
